e2ac954f7049791534ce8dc0eb2bb353c05d3816
Vinicius-dev <viniciusgabfarias123@gmail.com>
2023-09-15 10:56:08 -0300

    Feature: Aula 01 - 10 => Service, Components, Modules, Requisições HTTP, Angular Material, manipulando informações entre html e ts.

---
+++ crud-angular/src/app/courses/courses.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+  ]
+})
+export class CoursesModule { }
